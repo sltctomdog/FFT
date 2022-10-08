69f02be3ec53667a2564899a9105f55f2e1a72d3
sltctom <59124782+tom-Shiiir0@users.noreply.github.com>
2022-10-08 15:14:36 +0200

    TF1D complete

--- FFT.c
+++ FFT.c
@@ -3,7 +3,7 @@
 #include <complex.h>
 #include <math.h>
 
-const double Image1D[]={30,20,10,12,5};
+const int Image1D[]={1,6,2,4};
 
 const double Image2D[3][3]={
     {0.1,0.8,1},
@@ -11,35 +11,34 @@ const double Image2D[3][3]={
     {0.4,0.7,0.9}
     };
 
-void T1D();
-void T1DInverse();
-void T2D();
-void T2DInverse();
-void AfficherMatric1D(double complex Image[], int size);
+void TF1D();
+void TFInverse1D();
+void TF2D(); 
+void TFInverse2D();
 
 int main(int argc, char const *argv[])
 {
     int choix;   
 
     printf("Choisir la transformee de fourrier a utiliser : \n (1)  1D\n (11) 1D Inverse\n (2)  2D\n (22) 2D Inverse\n");
-    //scanf("%d",&choix);
+    scanf("%d",&choix);
     choix=1;
     switch(choix)
     {
         case 1:
-            T1D();
+            TF1D();
             break;
 
         case 11:
-            T1DInverse();
+            TFInverse1D();
             break;
 
         case 2:
-            T2D();
+            TF2D();
             break;
 
         case 22:
-            T2DInverse();
+            TFInverse2D();
             break;
 
         default:
@@ -48,49 +47,48 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-void T1D(){
-    int N=sizeof(Image1D)/sizeof(double);
+void TF1D(){
+    //Longueur du tableau
+    unsigned short N = *(&Image1D + 1) - Image1D;
+    //Notre tableau vide de retour
     double complex ImageRes[N];
+    //Notre somme
+    double complex sum;
 
-    for(int u=0;u<N-1;u++)
-    {      
-        double sum=0; 
-        double complex z;     
-        for(int x=0;x<N-1;x++)
+    //On parcours notre tableau initial
+    for(int u=0;u<N;u++)
+    {     
+        sum=0;   
+        //On applique la formule 
+        for(int x=0;x<N;x++)
         {
-            z = (-2 * I * M_PI * u * x) / N;
-            z+=Image1D[x]*cexp(z);
+            sum+=Image1D[x]*cexp((-2 * I * M_PI * u * x) / N);
         }
-        ImageRes[u]=z;
+        ImageRes[u]=sum;
     }
 
-   AfficherMatric1D(ImageRes,N);
-}
-
-void T1DInverse(){
-    
-}
-
-void T2D(){
-    
-}
-
-void T2DInverse(){
-    
-}
-
-void AfficherMatric1D(double complex Image[], int size)
-{
-    int N=size;
-    for(int i=0;i<N-1;i++)
-    {
-        if(i==0)
-            printf("[%f + %f I, ", creal(Image[i]),cimag(Image[i]));        
-        if(i==N-2)
-            printf("%f + %f I] ", creal(Image[i]),cimag(Image[i]));        
+    //Affichage Matrice
+    printf("[ ");
+    for(int i=0;i<N;i++)
+    {       
+        if(cimag(ImageRes[i])<0)
+            printf("(%.2f%.2fi), ",creal(ImageRes[i]),cimag(ImageRes[i]));      
         else
-            printf("%f + %f I, ", creal(Image[i]),cimag(Image[i]));        
-    }
+            printf("(%.2f+%.2fi), ",creal(ImageRes[i]),cimag(ImageRes[i]));         
+   }
+   printf("]\n");
+}
+
+void TFInverse1D(){
+    
+}
+
+void TF2D(){
+    
+}
+
+void TFInverse2D(){
+    
 }
 
 
