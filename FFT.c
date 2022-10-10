76524a09d45498b7e740c41e99be446d39695bac
Branedon <60399568+Branedon@users.noreply.github.com>
2022-10-10 16:16:30 +0200

    TF1D + afficheTF1D

--- FFT.c
+++ FFT.c
@@ -2,31 +2,34 @@
 #include <stdlib.h>
 #include <complex.h>
 #include <math.h>
+#include <string.h>
 
-const int Image1D[]={1,6,2,4};
+int Image1D[]={1,6,2,4};
 
-const double Image2D[3][3]={
+double Image2D[3][3]={
     {0.1,0.8,1},
     {0.3,0.5,0.7},
     {0.4,0.7,0.9}
     };
 
-void TF1D();
+double complex* TF1D(int* mat1D, int dim);
+void afficheTF1D(double complex* mat1D, int dim);
 void TFInverse1D();
 void TF2D(); 
 void TFInverse2D();
 
 int main(int argc, char const *argv[])
 {
+    double complex* matTF1D;
     int choix;   
-
     printf("Choisir la transformee de fourrier a utiliser : \n (1)  1D\n (11) 1D Inverse\n (2)  2D\n (22) 2D Inverse\n");
     scanf("%d",&choix);
     choix=1;
     switch(choix)
     {
-        case 1:
-            TF1D();
+        case 1:         
+            matTF1D = TF1D(Image1D, 4);
+            afficheTF1D(matTF1D, 4);
             break;
 
         case 11:
@@ -47,37 +50,42 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-void TF1D(){
+double complex* TF1D(int* mat1D, int dim){
+
+    double complex* matTF1D = (double complex*) malloc(dim*sizeof(double complex));
+
     //Longueur du tableau
-    unsigned short N = *(&Image1D + 1) - Image1D;
-    //Notre tableau vide de retour
-    double complex ImageRes[N];
+    int N = dim;
     //Notre somme
     double complex sum;
 
     //On parcours notre tableau initial
-    for(int u=0;u<N;u++)
+    for(int u=0;u<dim;u++)
     {     
         sum=0;   
         //On applique la formule 
-        for(int x=0;x<N;x++)
+        for(int x=0;x<dim;x++)
         {
-            sum+=Image1D[x]*cexp((-2 * I * M_PI * u * x) / N);
+            sum+=mat1D[x]*cexp((-2 * I * M_PI * u * x) / dim);
         }
-        ImageRes[u]=sum;
+        matTF1D[u]=sum;
     }
+    return matTF1D;
+}
 
+void afficheTF1D(double complex* mat1D, int dim){
     //Affichage Matrice
     printf("[ ");
-    for(int i=0;i<N;i++)
+    for(int i=0;i<dim;i++)
     {       
-        if(cimag(ImageRes[i])<0)
-            printf("(%.2f%.2fi), ",creal(ImageRes[i]),cimag(ImageRes[i]));      
+        if(cimag(mat1D[i])<0)
+            printf("(%.2f%.2fi), ",creal(mat1D[i]),cimag(mat1D[i]));      
         else
-            printf("(%.2f+%.2fi), ",creal(ImageRes[i]),cimag(ImageRes[i]));         
+            printf("(%.2f+%.2fi), ",creal(mat1D[i]),cimag(mat1D[i]));         
    }
    printf("]\n");
 }
+
 
 void TFInverse1D(){
     
